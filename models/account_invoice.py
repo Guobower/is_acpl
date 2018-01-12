518ab7a109d49c859efead8d2e5c20d34159737f
Tony Galmiche <tony.galmiche@infosaone.com>
2018-01-12 17:52:36 +0100

    add type_facture

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -6,3 +6,8 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     is_date_intervention = fields.Char(string="Date(s) d'intervention(s)")
+    is_type_facture      = fields.Selection([
+        ('atelier', u'Atelier'),
+        ('controle-analogique', u'Contrôle analogique'),
+        ('controle-numerique' , u'Contrôle numérique')
+    ], "Type de facture", required=True, default='atelier')
